7cd7e272d4b39c1281410aad5fa4014a64059776
unknown <agliullin61@gmail.com>
2025-09-26 14:04:04 +0300

    FilterRepaceCertificate

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\RotateMisassignedCertificates;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Явленная регистрация команды ротации сертификатов
+Artisan::command('certificates:rotate-misassigned {--apply : Apply changes instead of dry-run}', function () {
+    /** @var RotateMisassignedCertificates $cmd */
+    $cmd = app(RotateMisassignedCertificates::class);
+    return $cmd->handle();
+})->purpose('Перекинуть сертификаты у людей со статусом "Не работающий" с поддержкой --apply');
